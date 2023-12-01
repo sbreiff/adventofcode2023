e302bf4c4cdcd34f624e84303f232588ce6bd207
Sarah Reiff <sbreiff@gmail.com>
2023-12-01 15:55:19 -0500

    removed commented out code + repeated dict

--- day1/day1.py
+++ day1/day1.py
@@ -21,21 +21,11 @@ def find_digit(text_input, words=False):
         
 
 def get_numbers(text_input, words=False):
-    number_words = {
-        'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 
-        'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9', 
-        'ten': '10', 'eleven': '11', 'twelve': '12', 'thirteen': '13', 'fourteen': '14', 
-        'fifteen': '15', 'sixteen': '16', 'seventeen': '17', 'eighteen': '18', 'nineteen': '19',
-        'twenty': '20', 'thirty': '30', 'forty': '40', 'fifty': '50', 
-        'sixty': '60', 'seventy': '70', 'eighty': '80', 'ninety': '90', 'hundred': '100' 
-    }
     first_digit = None
     last_digit = None
-    #print(text_input)
     for i in range(1, len(text_input) + 1):
         start = text_input[:i]
         end = text_input[len(text_input)-i:]
-        #print(start, end)
         if not first_digit:
             first_digit = find_digit(start, words=words)
         if not last_digit:
